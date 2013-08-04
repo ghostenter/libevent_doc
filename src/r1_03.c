f30fdc581049bb96dfb80aff8bf30fac1587fd98
xuchdong <xuchdong@gmail.com>
2013-08-04 15:20:59 +0800

    add: 完成第一章

---
+++ src/r1_03.c
@@ -0,0 +1,15 @@
+#include <event2/event.h>
+
+#if !defined(LIBEVENT_VERSION_NUMBER) || LIBEVENT_VERSION_NUMBER < 0x02000100
+#error "This version of Libevent is not supported; Get 2.0.1-alpha or later"
+#endif
+
+int make_sandwich(void)
+{
+#if LIBEVENT_VERSION_NUMBER >= 0x06000500
+    evutil_make_me_a_sandwich();
+    return 0;
+#else
+    return -1;
+#endif;
+}
---
+++ src/r1_snip_07.c
@@ -0,0 +1,4 @@
+#define LIBEVENT_VERSION_NUMBER 0x02000300
+#define LIBEVENT_VERSION "2.0.3-alph"
+const char *event_get_version(void);
+ev_uint32_t event_get_version_number(void);
